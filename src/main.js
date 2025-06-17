d22eae31158fb4a13a759bfb8bdd28fed10f892f
Networksentinel <network.sentinel@protonmail.com>
2025-06-17 14:02:51 +0200

    feat: added transition classes for the tooltip + in JS, declared functions to hide/remove those classes and used them in the eventlistener click

--- src/main.js
+++ src/main.js
@@ -4,8 +4,23 @@ const shareButton = document.getElementById('share-button');
 const shareIcon = document.getElementById('share-icon');
 const tooltip = document.getElementById('tooltip');
 
+function showTooltip() {
+  tooltip.classList.remove('opacity-0', 'invisible');
+  tooltip.classList.add('opacity-100', 'visible');
+}
+
+function hideTooltip() {
+  tooltip.classList.remove('opacity-100', 'visible');
+  tooltip.classList.add('opacity-0', 'invisible');
+}
+
 shareButton.addEventListener('click', () => {
   shareButton.classList.toggle('bg-gray-500');
   shareIcon.classList.toggle('text-white');
-  tooltip.classList.toggle('hidden');
+
+    if (tooltip.classList.contains('invisible')) {
+        showTooltip();
+    } else {
+        hideTooltip();
+    }
 });
