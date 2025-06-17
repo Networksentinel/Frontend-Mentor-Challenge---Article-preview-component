94c0c8c19a0dea128080e474768b17466ab31e9a
Networksentinel <network.sentinel@protonmail.com>
2025-06-17 13:46:14 +0200

    feat/fix: added CSS variables back in as they tailwind default colors are different + added JS to display the tooltip and adjust button styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+const shareButton = document.getElementById('share-button');
+const shareIcon = document.getElementById('share-icon');
+const tooltip = document.getElementById('tooltip');
+
+shareButton.addEventListener('click', () => {
+  shareButton.classList.toggle('bg-gray-500');
+  shareIcon.classList.toggle('text-white');
+  tooltip.classList.toggle('hidden');
+});
